a323f707b0684251166fb74ad350aef471f47055
Doug Colaizzo <dcolaizzo@gmail.com>
2015-06-22 14:08:36 -0400

    Inital commit with working template

---
+++ html5-video-autoplay-on-hover.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var videoTags = document.getElementsByTagName('video');
+    for (var i = 0; i < videoTags.length; i++) {
+        var videoTag = videoTags[i];
+        videoTag.addEventListener("mouseover", function(e) {
+            e.target.play();
+        })
+        videoTag.addEventListener("mouseout", function(e) {
+            e.target.pause();
+        })
+    }
+};
