4e5bbbee0311c67327bfb17f4df1ac7b2b476e2b
Doug Colaizzo <dcolaizzo@gmail.com>
2015-06-24 15:47:16 -0400

    Messing with data

--- descriptors.js
+++ descriptors.js
@@ -3,20 +3,21 @@ window.addEventListener("load", function() {
         height = width;
     radius = Math.min(width, height) / 2;
 
-    var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-    var outerArcOuterRadius = radius - radius / 10,
-        outerArcInnerRadius = radius - radius / 2.1,
-        innerArcOuterRadius = radius - radius / 1.9,
-        innerArcInnerRadius = radius - radius / 1.2;
+    var outerArcOuterRadius = radius - radius / 11,
+        outerArcInnerRadius = radius - radius / 3,
+        innerArcOuterRadius = radius - radius / 2.7,
+        innerArcInnerRadius = radius - radius / 1.5;
 
     var outerArc = d3.svg.arc()
         .outerRadius(outerArcOuterRadius)
-        .innerRadius(outerArcInnerRadius);
+        .innerRadius(outerArcInnerRadius)
+        .startAngle(Math.PI * 3/4)
+        .endAngle(-Math.PI * 3/4);
     var innerArc = d3.svg.arc()
         .outerRadius(innerArcOuterRadius)
-        .innerRadius(innerArcInnerRadius);
+        .innerRadius(innerArcInnerRadius)
+        .startAngle(Math.PI * 3/4)
+        .endAngle(-Math.PI * 3/4);
 
     var pie = d3.layout.pie()
         .sort(null)
@@ -31,26 +32,26 @@ window.addEventListener("load", function() {
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var outerData = [{
-        age: "<5",
-        population: 2704659
+        age: "Goal Driven",
+        population: 100
     }, {
         age: "5-13",
-        population: 4499890
+        population: 100
     }, {
         age: "14-17",
-        population: 2159981
+        population: 100
     }, {
         age: "18-24",
-        population: 3853788
+        population: 100
     }, {
         age: "25-44",
-        population: 14106543
+        population: 100
     }, {
         age: "45-64",
-        population: 8819342
+        population: 100
     }, {
-        age: "45-64",
-        population: 612463
+        age: "Creative",
+        population: 100
     }]
 
     outerData.forEach(function(d) {
